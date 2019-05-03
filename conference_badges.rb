6d8244e45b1476e701007229e7565b02660b9e6c
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-03 00:16:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,13 @@
 # Write your code here.
 def badge_maker(name)
-  name.each do |people|
-  puts "Hello, my name is #{people}."
+    "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badge_messages = [ ]
+  attendees.each do
+    |people|
+    badge_messages<<"Hello, my name is #{people}."
+  end
+  return badge_messages
 end
